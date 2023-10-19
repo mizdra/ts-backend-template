3e4d3a4a2ac57fcec31cb81cbac9c5aac5b266e6
mizdra <pp.mizdra@gmail.com>
2023-10-19 23:15:29 +0900

    graphql エンドポイントを生やす

---
+++ src/lib/app.ts
@@ -0,0 +1,12 @@
+import { Hono } from 'hono';
+import { yoga } from './graphql-server.js';
+
+export const app = new Hono();
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
+});
+
+app.use('/graphql', async (context) => {
+  return yoga.handle(context.req.raw, context);
+});
