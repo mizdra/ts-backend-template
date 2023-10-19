aa6501521109a4aea22d8086b73ede3af73b7c42
mizdra <pp.mizdra@gmail.com>
2023-10-20 00:29:59 +0900

    テストがコケる問題を修正

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,7 +1,11 @@
 import { writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { printSchema, lexicographicSortSchema } from 'graphql';
+// graphql package には、graphql の異なるバージョンを複数 import すると実行時エラーを出す機能がある (例: v15 と v14 どちらも import するなど)。
+// このエラーは同じバージョンであったとしても、ESM版とCJS版を同時に import した場合にも発動する。
+// pothos や graphql-yoga では `graphql/index.js` (CJS版) が import されているようなので、実行時エラーを避けるべく、ここでも同様に CJS版 を import してる。
+// ref: https://github.com/graphql/graphql-js/issues/594#issuecomment-926683870
+import { printSchema, lexicographicSortSchema } from 'graphql/index.js';
 import { builder } from './builder.js';
 
 builder.queryType({
