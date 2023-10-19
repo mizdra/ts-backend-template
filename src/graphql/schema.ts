294d74b15bdbef2834baba8eeee9dbbb24e55c4f
mizdra <pp.mizdra@gmail.com>
2023-10-20 00:14:49 +0900

    スキーマファイルは書き出されていたほうがレビューなどに役立つのでそうする

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,3 +1,7 @@
+import { writeFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { printSchema, lexicographicSortSchema } from 'graphql';
 import { builder } from './builder.js';
 
 builder.queryType({
@@ -12,3 +16,8 @@ builder.queryType({
 });
 
 export const schema = builder.toSchema({});
+
+export async function writeSchemaFile() {
+  const schemaAsString = printSchema(lexicographicSortSchema(schema));
+  await writeFile(join(fileURLToPath(import.meta.url), '../../../schema.graphql'), schemaAsString);
+}
