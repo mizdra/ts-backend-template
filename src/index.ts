5d4e52512f81bbb5d6424e460981a76b7e87518c
mizdra <pp.mizdra@gmail.com>
2023-10-19 19:48:37 +0900

    graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,13 @@ app.get('/', (c) => {
   return c.text('Hello Hono!');
 });
 
-serve({ fetch: app.fetch, port: 3000 });
+const server = serve({ fetch: app.fetch, port: 3000 });
 // eslint-disable-next-line no-console
 console.log('Server is running on http://localhost:3000');
+
+// Graceful shutdown
+server.on('close', () => {
+  // TODO: disconnect from database
+});
+process.on('SIGINT', () => server.close());
+process.on('SIGTERM', () => server.close());
