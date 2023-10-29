8cc61115864a91d26ff735061ae91a26335b628e
mizdra <pp.mizdra@gmail.com>
2023-10-30 01:39:54 +0900

    @pothos/plugin-prisma が使えるように

--- src/graphql/builder.ts
+++ src/graphql/builder.ts
@@ -1,3 +1,16 @@
 import SchemaBuilder from '@pothos/core';
+// eslint-disable-next-line import/no-named-as-default
+import PrismaPlugin from '@pothos/plugin-prisma';
+import type PrismaTypes from '@pothos/plugin-prisma/generated';
+import { prisma } from '../prisma/client.js';
 
-export const builder = new SchemaBuilder({});
+export const builder = new SchemaBuilder<{
+  PrismaTypes: PrismaTypes;
+}>({
+  plugins: [PrismaPlugin],
+  prisma: {
+    client: prisma,
+    filterConnectionTotalCount: true,
+    onUnusedQuery: process.env['NODE_ENV'] === 'production' ? null : 'warn',
+  },
+});
